b7ac9a3980c8d7bf0f9ad0883f42c3037e7df6f3
Timothy Dao <timster78811@gmail.com>
2018-07-19 16:35:39 -0400

    Finish shopping list

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
+/* global $ */
 $(function() {
-	$('#js-shopping-list-form').submit(function(event) {
-		event.preventDefault();
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
     
-		let shoppingItem = $('.shopping-list-entry').val();
-		$('.shopping-list').append(
-			`<li>
+    let shoppingItem = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    $('.shopping-list').append(
+      `<li>
       <span class="shopping-item">${shoppingItem}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle">
@@ -14,12 +17,21 @@ $(function() {
           <span class="button-label">delete</span>
         </button>
       </div>
-    </li>`
-    )
-  })
+    </li>`);
+  });
 
-	$('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-		$(this).closest('li').remove();
-	})
-}
-);
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
+
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+    $(this).closest('li').toggleClass('shopping-item__checked');
+    
+  });
+  
+    
+  
+  
+});
+
+
