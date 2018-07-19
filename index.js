810722193d550536efbb290912d7ecec71cd7f3e
Timothy Dao <timster78811@gmail.com>
2018-07-19 17:31:25 -0400

    final change

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ $(function() {
   });
 
   $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
-    $(this).closest('li').toggleClass('shopping-item__checked');
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
     
   });
   
